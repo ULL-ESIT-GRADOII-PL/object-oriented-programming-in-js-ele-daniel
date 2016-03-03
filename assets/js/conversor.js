ea88382c1d7b120b5e4ddc3d8510a7e0d3941814
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-03-03 22:53:51 +0000

    adding XRegexExp

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,58 +1,104 @@
 (function(exports) {
-  "use strict";
+    "use strict";
 
-  function Medida(valor,tipo)  
-  {
+
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
-  }
-  
-  function Temperatura(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
-  }
-  
-  function Celsius(valor)
-  {
-  }
-  
-  function Farenheit(valor)
-  {
-  }
-  
-  exports.Temperatura = Temperatura;
-  exports.Celsius = Celsius;
-  exports.Farenheit = Farenheit;
-
-  exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
-    valor     = valor.match(regexp);
-    
-    if (valor) {
-      var numero = valor[1],
-          tipo   = valor[2].toLowerCase();
-      
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
-      switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          break;
-        
-        default:
-          /* rellene este código */
-      }
+    function Medida(valor,tipo) {
+        if (!tipo) {
+            var param = XRegExp(""
+                                + "(?<value>       [-+]?\\d+ (?:[\\.,]\\d*)?\\s* ) # Get number \n"
+                                + "((e(?<exponent> [-+]?\\d+)\\s*)?)               # Get Exponent \n"
+                                + "(?<measure>     [a-zA-Z]+)                      # Get kind");
+            var m = XRegexExp.exec(valor, param);
+            this.valor = parseFloat(m.value) * Math.pow(10, parseInt(m.exponent));
+            this.tipo  = m.measure;
+        }
+        else {
+            this.valor = valor;
+            this.tipo  = tipo;
+        }
     }
-    else
-      elemento.innerHTML = "";
-  };
+
+    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    function Temperatura(valor,tipo) {
+        Medida.call(this, valor, tipo);
+    }
+
+    function Celsius(valor) {
+        Temperatura.call(this, valor, "c");
+    }
+
+    Celsius.prototype = {
+        toFahrenheit: function(value) {
+            return ((this.valor * 9/5)+32);
+        }
+    }
+
+    function Fahrenheit(valor) {
+        Temperatura.call(this, valor, "f");
+    }
+
+    Fahrenheit.prototype = {
+        toCelsius: function(value) {
+            return ((this.valor - 32)*5/9);
+        }
+    }
+
+    exports.Temperatura = Temperatura;
+    exports.Celsius = Celsius;
+    exports.Fahrenheit = Fahrenheit;
+
+    // Tengo la expresion general parseo un entero en ya sea en notacion
+    // cientifica o no
+    // y algun tipo de conversion
+    // despues tener un array y recorrerlo aplicando el metodo parse measure hasta encontrar uno valido
+
+
+    // Claro lo de arriba puede ir mal con lo que añadir parsers para las opciones 30f to c, 30f c
+    exports.convertir = function() {
+        var valor     = document.getElementById('convert').value,
+            elemento  = document.getElementById('converted');
+
+        var measures = "((f(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?)"
+            + "|(k(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?)"
+            + "|(c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?))";
+
+        var inputRegex = XRegExp (""
+                                  + '^(\\s*)                                         # whitespaces \n'
+                                  + '(?<value>       [-+]?\\d+ (?:[\\.,]\\d*)?\\s*)  # captures the number   \n'
+                                  + '((e(?<exponent> [-+]?\\d+)\\s*)?)               # captures the exponent \n'
+                                  + '(?<kind>       ' + measures + ')                # Capture kind of value \n'
+                                  + '(\\s*)$                                         # whitespaces \n'
+                                  , 'xi');
+
+        valor = XRegExp.exec(valor, inputRegex);
+
+        if (valor) {
+            var numero = parseFloat(valor.value), // Int
+                tipo   = valor.kind;           // String
+
+            console.log("Valor: " + numero + ", Tipo: " + tipo);
+
+            switch (tipo) {
+            case 'c':
+                var celsius = new Celsius(numero);
+                elemento.innerHTML = celsius.toFahrenheit().toFixed(2) + " Farenheit";
+                break;
+            case 'f':
+                var fahrenheit = new Fahrenheit(numero);
+                elemento.innerHTML = fahrenheit.toCelsius().toFixed(2) + " Celsius";
+                break;
+            // case 'k':
+            //     var kelvin = new Kelvin(numero);
+            //     elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
+            //     break;
+
+            default:
+                /* rellene este código */
+            }
+        }
+        else
+            elemento.innerHTML = "";
+    };
 })(this);
